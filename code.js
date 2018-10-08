9fd19bc1e64412f80b1ade5dae9a742512199ec2
boxingjellyfish <tute1985@adinet.com.uy>
2018-10-08 15:52:41 -0300

    Engine class

--- code.js
+++ code.js
@@ -1,15 +1,18 @@
 var canvas = document.getElementById("canvas");
-var width = canvas.width;
-var height = canvas.height;
 var ctx = canvas.getContext("2d");
 
-var physicObjects = [];
+var world = {
+    width: canvas.width,
+    height: canvas.height,
+    physicObjects: []
+};
+
 for (var i = 0; i < 100; i++) {
-    physicObjects.push({
+    world.physicObjects.push({
         id: i,
         position: {
-            x: Math.random() * width,
-            y: Math.random() * height
+            x: Math.random() * world.width,
+            y: Math.random() * world.height
         },
         velocity: {
             x: Math.random() * Math.random(),
@@ -22,63 +25,80 @@ for (var i = 0; i < 100; i++) {
             s: 80,
             l: 50,
             a: 0.8
-        }
+        },
+        visible: true,
+        deleted: false
     });
 }
 
 function update(delta) {
-    for (var i = 0; i < physicObjects.length; i++) {
-        var box = physicObjects[i];
-        if ((box.position.x + box.width > width && box.velocity.x > 0) || (box.position.x < 0 && box.velocity.x < 0)) {
+    for (var i = 0; i < world.physicObjects.length; i++) {
+        var box = world.physicObjects[i];
+        if ((box.position.x + box.width > world.width && box.velocity.x > 0) || (box.position.x < 0 && box.velocity.x < 0)) {
             box.velocity.x *= -1;
         }
-        if ((box.position.y + box.height > height && box.velocity.y > 0) || (box.position.y < 0 && box.velocity.y < 0)) {
+        if ((box.position.y + box.height > world.height && box.velocity.y > 0) || (box.position.y < 0 && box.velocity.y < 0)) {
             box.velocity.y *= -1;
         }
-        for (var j = 0; j < physicObjects.length; j++) {
-            var other_box = physicObjects[j];
-            if(box.id != other_box.id) {
+        for (var j = 0; j < world.physicObjects.length; j++) {
+            var other_box = world.physicObjects[j];
+            if (box.id != other_box.id && box.visible && other_box.visible) {
                 if (box.position.x < other_box.position.x + other_box.width &&
                     box.position.x + box.width > other_box.position.x &&
                     box.position.y < other_box.position.y + other_box.height &&
                     box.height + box.position.y > other_box.position.y) {
-                        box.velocity.x *= -1;
-                        box.velocity.y *= -1;
-                 }
+                    //box.velocity.x *= -1;
+                    //box.velocity.y *= -1;
+                    //box.visible = false;
+                    //other_box.visible = false;
+                    if (box.width * box.height >= other_box.width * other_box.height) {
+                        other_box.visible = false;
+                        box.width += 5;
+                        box.height += 5;
+                    }
+                }
             }
         }
     }
-    for (var i = 0; i < physicObjects.length; i++) {        
-        var box = physicObjects[i];
-        box.position.x += box.velocity.x * delta;
-        box.position.y += box.velocity.y * delta;
+    for (var i = world.physicObjects.length - 1; i >= 0; i--) {
+        var box = world.physicObjects[i];
+        if (!box.deleted) {
+            box.position.x += box.velocity.x * delta;
+            box.position.y += box.velocity.y * delta;
+        }
+        else {
+            world.physicObjects.splice(i, 1);
+        }
     }
 }
 
 function draw(interp) {
-    ctx.clearRect(0, 0, width, height);
+    ctx.clearRect(0, 0, world.width, world.height);
     ctx.save();
 
-    for (var i = 0; i < physicObjects.length; i++) {
-        var box = physicObjects[i];
-        ctx.fillStyle = "hsla(" + box.color.h + ", " + box.color.s + "%, " + box.color.l + "%, " + box.color.a + ")";
-        ctx.fillRect(box.position.x, box.position.y, box.width, box.height);
+    for (var i = 0; i < world.physicObjects.length; i++) {
+        var box = world.physicObjects[i];
+        if (box.visible) {
+            ctx.fillStyle = "hsla(" + box.color.h + ", " + box.color.s + "%, " + box.color.l + "%, " + box.color.a + ")";
+            ctx.fillRect(box.position.x, box.position.y, box.width, box.height);
+        }
     }
 
     ctx.fillStyle = "darkgrey";
     ctx.font = "12px monospace";
-    ctx.fillText(Math.round(MainLoop.getFPS()) + " FPS", 1, 10);
+    ctx.fillText(Math.round(loop.getFPS()) + " FPS", 1, 10);
+    ctx.fillText(world.physicObjects.length + " objects", 1, 22);
 
     ctx.restore();
 }
 
 function toggle() {
-    if (MainLoop.isRunning()) {
-        MainLoop.stop();
+    if (loop.isRunning()) {
+        loop.stop();
     }
     else {
-        MainLoop.start();
+        loop.start();
     }
 }
 
-MainLoop.setUpdate(update).setDraw(draw).start();
+var loop = new MainLoop().setUpdate(update).setDraw(draw).start();
